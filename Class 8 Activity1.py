ec482f54c62fbbb777acb3ea88a6926be0235d1f
abbymagicwhisper <aayanbl11@gmail.com>
2025-12-26 18:16:06 +0530

    Add files via upload

---
+++ Class 8 Activity1.py
@@ -0,0 +1,16 @@
+w = 45
+v = 23
+x = 4
+y = 3
+z = (w + v - x)/y
+a = (z * 3) ** 2
+print("The value of z is", z)
+print("The value of a is", a)
+
+name = "Joshua"
+age = 14
+
+if name == "Joshua" and age >= 11 or name == "Emily" and age >= 12:
+    print("Hello, Joshua!, You have ran this program successfully")
+else:
+    print("Access denied")
